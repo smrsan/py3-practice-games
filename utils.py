db456c1efcd3464db5fbdc93206ad2bfe9df28eb
smrsan <smrsan@users.noreply.github.com>
2019-11-15 20:25:45 +0330

    Added utils.py

---
+++ utils.py
@@ -0,0 +1,24 @@
+import os
+
+
+def get_item_by_index(obj, key_index, default_val='quit'):
+    keys_list = list(obj.keys())
+    result_key = keys_list[key_index] if 0 <= key_index < len(
+        keys_list) else default_val
+    result_val = obj.get(result_key, default_val)
+    return result_key, result_val
+
+
+def is_numeric(some_str):
+    try:
+        int(some_str)
+        return True
+    except ValueError:
+        return False
+
+
+def clear_term():
+    # sys.stderr.write("\x1b[2J\x1b[H")
+    # print(chr(27) + "[2J")
+    # print("\x1b[2J\x1b[H")
+    os.system('cls' if os.name == 'nt' else 'clear')
